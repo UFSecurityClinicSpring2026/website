5748d905fc959df07071864f2e3eb440300a2b5e
maxgriff <maxwellgriffin@ufl.edu>
2026-02-24 20:38:04 -0500

    Added a functional register page for main website

--- docker/website/webapp/app.py
+++ docker/website/webapp/app.py
@@ -4,7 +4,13 @@ import flask
 
 import sqldb
 
+import werkzeug.security as wk
+import re
+
 app: flask.Flask = flask.Flask(__name__)
+
+def check_requirements(username: str, password: str):
+  return re.match(".{15,64}", password) is not None and re.match("\\w+", username) is not None
 
 @app.route("/")
 def index():
@@ -45,3 +51,45 @@ def new_ticket():
         )
     else:
         flask.abort(405)
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if flask.request.method == "GET":
+        return flask.render_template("register.html")
+    elif flask.request.method == "POST":
+        sql_db: sqlite3.Connection = sqldb.get_db()
+        if "username" not in flask.request.form or "first_name" not in flask.request.form or \
+                "last_name" not in flask.request.form or "password" not in flask.request.form or \
+                "selection" not in flask.request.form:
+            flask.abort(422)
+        username, password = flask.request.form["username"].strip(), flask.request.form["password"].strip()
+        is_client = 0
+        is_student = 0
+        if flask.request.form["selection"] == "client":
+            is_client = 1
+        elif flask.request.form["selection"] == "student":
+            is_student = 1
+        else:
+            print("bad selection")
+            flask.abort(422)
+        if not check_requirements(username, password):
+          # Should have a check in frontend as well, but okay for now
+            flask.abort(422)
+        hashed = wk.generate_password_hash(password, salt_length=32)
+        res = sql_db.execute("SELECT username FROM users WHERE username = ?;", (username,))
+        res = res.fetchall()
+        if len(res) > 0:
+            flask.abort(422)
+        sql_db.execute("INSERT INTO users (username, first_name, last_name, auth_string, is_student, is_client) VALUES (?, ?, ?, ?, ?, ?);", \
+                        (username, flask.request.form["first_name"], flask.request.form["last_name"], \
+                         hashed, is_student, is_client))
+        sql_db.commit()
+        return flask.render_template(
+            "delayed-redirect.html",
+            title="Registered Successfully!",
+            delay=5,
+            message="You have successfully registered as a " + flask.request.form["selection"],
+            destination="/"
+        )
+    else:
+      flask.abort(405)
--- docker/website/webapp/sqldb.py
+++ docker/website/webapp/sqldb.py
@@ -23,6 +23,7 @@ def get_db() -> sqlite3.Connection:
     if "db" not in flask.g:
         flask.g.db = sqlite3.connect(database_path)
         flask.g.db.execute("CREATE TABLE IF NOT EXISTS tickets (fid INTEGER NOT NULL, name TEXT NOT NULL, email TEXT NOT NULL, message TEXT, PRIMARY KEY (fid AUTOINCREMENT));")
+        flask.g.db.execute("CREATE TABLE IF NOT EXISTS users (uid INTEGER NOT NULL, username TEXT NOT NULL, first_name TEXT NOT NULL, last_name TEXT NOT NULL, auth_string TEXT NOT NULL, is_student INTEGER, is_client INTEGER, PRIMARY KEY (uid AUTOINCREMENT));")
         flask.g.db.commit()
     
     return flask.g.db
